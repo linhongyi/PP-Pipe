03f52a2bf0710de7591967f310054c40036e5969
linhongyi <linhongyi73@gmail.com>
2020-04-22 10:22:11 +0800

    back up router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { GridListComponent } from './grid-list/grid-list.component';
 import { BottomTableComponent } from './bottom-table/bottom-table.component';
 import { TabGroupComponent } from './tab-group/tab-group.component';
 import { CardViewComponent } from './card-view/card-view.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { IndexPageComponent } from './index-page/index-page.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, DemoMaterialModule],
-  declarations: [ AppComponent, HelloComponent, GridListComponent, BottomTableComponent, TabGroupComponent, CardViewComponent ],
+  declarations: [ AppComponent, HelloComponent, GridListComponent, BottomTableComponent, TabGroupComponent, CardViewComponent, LoginPageComponent, IndexPageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule {
