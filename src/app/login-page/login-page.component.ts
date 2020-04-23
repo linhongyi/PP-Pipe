88affcb600e1b22a7999c741c47199f6712c3866
linhongyi <linhongyi73@gmail.com>
2020-04-23 17:41:08 +0800

    after login, go to index

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -9,17 +10,20 @@ import { FormControl, Validators, FormGroup} from '@angular/forms';
 
 export class LoginPageComponent implements OnInit {
   
-  public loginModel: FormGroup = new FormGroup ({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl()});
+  // public loginModel: FormGroup = new FormGroup ({
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   password: new FormControl()});
 
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl();
   
 
-  constructor() { }
+  constructor(private router: Router) { 
+
+  }
 
   ngOnInit() {
+    
   }
   
   getErrorMessage() {
@@ -55,7 +59,8 @@ export class LoginPageComponent implements OnInit {
   onLogin()
   {
     var str = this.email.value +'\n'+this.password.value;
-
+    this.router.navigate(['IndexPage']);
+    
     window.alert(str);
   }
 }
