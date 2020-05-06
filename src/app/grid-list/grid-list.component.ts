c27f923f9b4e497f5548a943b34f328345add955
Howard.Lin <howard@penpower.com.tw>
2020-05-06 11:54:03 +0800

    grid-view layout by screen size

--- src/app/grid-list/grid-list.component.ts
+++ src/app/grid-list/grid-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
 
 export interface Tile {
   color: string;
@@ -14,17 +15,36 @@ export interface Tile {
 })
 
 export class GridListComponent implements OnInit {
+ 
+ cols:number = 4;
 
- tiles: Tile[] = [
-    {text: 'One', cols: 2, rows: 6, color: 'lightblue'},
-    {text: 'Two', cols: 2, rows: 6, color: 'lightgreen'},
-    {text: 'Three', cols: 2, rows: 6, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 6, color: '#DDBDF1'},
-  ];
+ tiles: Tile[];
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+      this.breakpointObserver.observe([Breakpoints.HandsetLandscape, Breakpoints.HandsetPortrait])
+      .subscribe(result => {
+        if(result.matches==true)
+        {
+           this.tiles = [
+            {text: 'One', cols: this.cols, rows: 6, color: 'lightblue'},
+            {text: 'Two', cols: this.cols, rows: 6, color: 'lightgreen'},
+            {text: 'Three', cols: this.cols, rows: 6, color: 'lightpink'},
+            {text: 'Four', cols: this.cols, rows: 6, color: '#DDBDF1'},
+          ];
+        }
+        else
+        {
+          this.tiles = [
+            {text: 'One', cols: 2, rows: 6, color: 'lightblue'},
+            {text: 'Two', cols: 2, rows: 6, color: 'lightgreen'},
+            {text: 'Three', cols: 2, rows: 6, color: 'lightpink'},
+            {text: 'Four', cols: 2, rows: 6, color: '#DDBDF1'},
+          ];
+        }
+    });
+    
   }
-
 }
